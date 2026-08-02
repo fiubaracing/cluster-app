a6c46bfaaa7fa5e55399de4d84ee9a03739d2d24
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2026-08-02 16:03:38 -0300

    feat: Endpoint annotation

--- src/api/shared/handlers/index.ts
+++ src/api/shared/handlers/index.ts
@@ -3,11 +3,73 @@ import { promisify } from "node:util";
 import { withLogging } from "./tracing";
 import { withErrorHandler } from "./exception";
 import { ApiHandler } from "../types/api";
+import * as yup from "yup";
 
 export const execFile = promisify(execFileNode);
-export const buildRequest = (handler: ApiHandler) => {
-	let req = handler;
-	req = withLogging(req);
-	req = withErrorHandler(req);
-	return req;
+
+export interface Middlewares {
+	logging?: boolean;
+	errorHandler?: boolean;
+}
+
+const wrapRequest = <T extends ApiHandler>(
+	handler: T,
+	{ logging = true, errorHandler = true }: Middlewares = {},
+) => {
+	let wrappedHandler: ApiHandler = function (
+		this: unknown,
+		req: Request,
+		...args: any[]
+	) {
+		return handler.call(this, req, ...args);
+	};
+
+	if (logging) wrappedHandler = withLogging(wrappedHandler);
+	if (errorHandler) wrappedHandler = withErrorHandler(wrappedHandler);
+
+	return wrappedHandler as T;
+};
+
+const createDecorator = (options: Middlewares = {}): MethodDecorator => {
+	return (_target, _propertyKey, descriptor) => {
+		const method = descriptor.value;
+		if (typeof method === "function") {
+			descriptor.value = wrapRequest(
+				method as ApiHandler,
+				options,
+			) as any;
+		}
+	};
+};
+
+export function Endpoint(options?: Middlewares): MethodDecorator;
+export function Endpoint(
+	target: object,
+	propertyKey: string | symbol,
+	descriptor: PropertyDescriptor,
+): void;
+export function Endpoint(...args: any[]) {
+	if (args.length === 3) {
+		return createDecorator()(args[0], args[1], args[2]);
+	}
+
+	return createDecorator(args[0] ?? {});
+}
+
+export const validator = async (
+	schema: yup.ObjectSchema<any>,
+	data: any,
+): Promise<any> => {
+	return await schema.validate(data, {
+		abortEarly: false,
+		stripUnknown: true,
+	});
+};
+
+export const parseJSON = async (req: Request): Promise<any> => {
+	try {
+		return await req.json();
+	} catch (e) {
+		return {};
+	}
 };
