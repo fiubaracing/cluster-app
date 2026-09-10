1040f020545a317f2752d7a7c53d2b8abcc82c6a
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2026-09-10 19:49:06 -0300

    refactor: use FindUserInContextUseCase

--- src/api/auth/application/usecases/validate-access.usecase.ts
+++ src/api/auth/application/usecases/validate-access.usecase.ts
@@ -2,12 +2,24 @@ import {
 	ValidateItemAccessBulkDTO,
 	ValidateItemAccessDTO,
 } from "../dtos/validate-access.dto";
-import context from "@/api/shared/infrastructure/config/store";
 import { PermissionSuffix } from "@/api/roles/domain/models/permission.model";
 import { Module } from "@/api/roles/domain/models/module.model";
 import { ForbiddenException } from "@/api/shared/infrastructure/exceptions/forbidden.exception";
+import { FindUserInContextUseCase } from "@/api/users/application/usecases/find-user-in-context.usecase";
+import { UserWithRolesPermissionsAndTeams } from "@/api/users/domain/models/user.model";
+import { logger } from "@/api/shared/infrastructure/config/logger";
+
+interface ValidateAccessUseCaseDependencies {
+    findUserInContextUseCase?: FindUserInContextUseCase;
+}
 
 export class ValidateAccessUseCase {
+    private readonly findUserInContextUseCase: FindUserInContextUseCase;
+
+    constructor(deps?: ValidateAccessUseCaseDependencies) {
+        this.findUserInContextUseCase = deps?.findUserInContextUseCase ?? new FindUserInContextUseCase();
+    }
+
     /**
      * Validates if the current user has access to a specific item based on module, permission, and ownership.
      * @param dto - The data transfer object containing module, permission, and optional ownership information.
@@ -15,9 +27,15 @@ export class ValidateAccessUseCase {
      * @returns A promise that resolves if the user has access, otherwise it throws an exception.
      */
 	public async execute(dto: ValidateItemAccessDTO): Promise<void> {
-		const hasModulePermission = this.hasModulePermission(dto.module, dto.permission);
-        const hasOwnership = dto.ownerUuid ? this.hasOwnership(new Set([dto.ownerUuid])) : true;
-        const hasTeamOwnership = dto.ownerTeamUuid ? this.hasTeamOwnership(new Set([dto.ownerTeamUuid])) : true;
+        logger.info(`Use case ValidateAccessUseCase started`);
+        const user: UserWithRolesPermissionsAndTeams | null = await this.findUserInContextUseCase.execute();
+        if (!user) {
+            throw new Error("User not found in context");
+        }
+
+		const hasModulePermission = this.hasModulePermission(user, dto.module, dto.permission);
+        const hasOwnership = dto.ownerUuid ? this.hasOwnership(user, new Set([dto.ownerUuid])) : true;
+        const hasTeamOwnership = dto.ownerTeamUuid ? this.hasTeamOwnership(user, new Set([dto.ownerTeamUuid])) : true;
 
         if (!hasModulePermission || !hasOwnership || !hasTeamOwnership) {
             throw new ForbiddenException(
@@ -25,6 +43,8 @@ export class ValidateAccessUseCase {
                 `User does not have permission ${dto.permission} for a item in module ${dto.module}`,
             );
         }
+
+        logger.info("Use case ValidateAccessUseCase completed successfully");
 	}
 
     /**
@@ -34,9 +54,16 @@ export class ValidateAccessUseCase {
      * @returns A promise that resolves if the user has access to all items, otherwise it throws an exception.
      */
 	public async executeBulk(dto: ValidateItemAccessBulkDTO): Promise<void> {
-        const hasModulePermission = this.hasModulePermission(dto.module, dto.permission);
-        const hasOwnership = dto.ownerUuids ? this.hasOwnership(new Set(dto.ownerUuids)) : true;
-        const hasTeamOwnership = dto.ownerTeamUuids ? this.hasTeamOwnership(new Set(dto.ownerTeamUuids)) : true;
+        logger.info(`Use case ValidateAccessUseCase in bulk started`);
+
+        const user: UserWithRolesPermissionsAndTeams | null = await this.findUserInContextUseCase.execute();
+        if (!user) {
+            throw new Error("User not found in context");
+        }
+
+        const hasModulePermission = this.hasModulePermission(user, dto.module, dto.permission);
+        const hasOwnership = dto.ownerUuids ? this.hasOwnership(user, new Set(dto.ownerUuids)) : true;
+        const hasTeamOwnership = dto.ownerTeamUuids ? this.hasTeamOwnership(user, new Set(dto.ownerTeamUuids)) : true;
 
         if (!hasModulePermission || !hasOwnership || !hasTeamOwnership) {
             throw new ForbiddenException(
@@ -44,20 +71,21 @@ export class ValidateAccessUseCase {
                 `User does not have permission ${dto.permission} for items in module ${dto.module}`,
             );
         }
+        logger.info("Use case ValidateAccessUseCase in bulk completed successfully");
 	}
 
-    private hasModulePermission(module: Module, permission: PermissionSuffix): boolean {
-        const permissions = context.store.user.permissions;
+    private hasModulePermission(user: UserWithRolesPermissionsAndTeams, module: Module, permission: PermissionSuffix): boolean {
+        const permissions = user.permissions;
         return permissions.hasSuffix(module, permission);
     }
 
-    private hasOwnership(ownerUuid: Set<string>) {
-        const userUuid = context.store.user.uuid;
+    private hasOwnership(user: UserWithRolesPermissionsAndTeams, ownerUuid: Set<string>) {
+        const userUuid = user.uuid;
         return ownerUuid.has(userUuid);
     }
 
-    private hasTeamOwnership(ownerTeamUuids: Set<string>) {
-        const userTeamUuids = context.store.user.teams;
+    private hasTeamOwnership(user: UserWithRolesPermissionsAndTeams, ownerTeamUuids: Set<string>) {
+        const userTeamUuids = user.teams;
         return ownerTeamUuids.values().some(teamUuid => userTeamUuids.has(teamUuid));
     }
 }
